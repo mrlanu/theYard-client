ccf132aaf80cce030fcba0b67e34fa7cd12805bb
Serhiy K <mrlanu@gmail.com>
2019-02-15 20:23:27 -0600

    Angular Material added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MaterialModule} from './material.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    MaterialModule,
+    BrowserAnimationsModule,
     BrowserModule,
     AppRoutingModule
   ],
